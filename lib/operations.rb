dc9c6445a6d2e3be418759ca9c36b7d4283ede34
Sean Balke <sean.balke@gmail.com>
2020-05-27 00:18:09 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 def unsafe?(speed)
   binding.pry
-  if speed > 60
-    true
-  elsif speed < 40
-    true
-  else
+  # if speed > 60
+  #   true
+  # elsif speed < 40
+  #   true
+  # else
     false
 end
 
